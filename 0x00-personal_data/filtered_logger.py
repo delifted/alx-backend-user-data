73bcbdc5c86323162c3a731e27bc5a85acdb6420
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2024-04-24 05:37:56 +0100

    Updates made to the function file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 A module that filters logs
 '''
 import re
+import logging
 from typing import List
 
 
@@ -21,3 +22,28 @@ def filter_datum(
     '''
     extract, replace = (patterns['extract'], patterns['replace'])
     return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''
+        Formats a LogRecord
+        '''
+        msg = super(RedactingFormatter, self).format(record)
+        txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return txt
+
+
+# if __name__ == '__main__':
+#     main()
