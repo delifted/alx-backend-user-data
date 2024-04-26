17388d2771967ff67ce542c7e5c01efd7bc7efe8
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2024-04-26 06:43:41 +0100

    Authentication file template and auth Class created

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+'''
+Authentication file
+'''
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    '''
+    Class Auth
+    '''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''
+        Checks if a path requires authentication
+        '''
+        return False
+    
+
+    def authorization_header(self, request=None) -> str:
+        '''
+        Gets the authorization header field from the request
+        '''
+        return None
+    
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''
+        Gets active user from the request
+        '''
+        return None
