a13621b2153185dcd2666e5087f2ce68ac87b201
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2024-04-26 16:19:07 +0100

    BasicAuthentication file implemented

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -21,9 +21,39 @@ class BasicAuth(Auth):
         for a basic authentication
         '''
         if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group('token')
+        return None
+
+    def decode_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        '''
+        Extracts the Base64 part of the Authorization header
+        for a basic authentication
+        '''
+        if type(authorization_header) == str:
             try:
                 res = base64.b64decode(extract_base64_authorization_header,
                                        validate=True)
                 return res.decode('utf-8')
             except (binascii.Error, UnicodeDecodeError):
                 return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        '''
+        Extracts user credentials from a base64-decoded authorization
+        header that uses the Basic authentication flow
+        '''
+        if type(decoded_base64_authorization_header) == str:
+            pattern = r'(?P<user>[^:]+):(?P<password>.+)'
+            field_match = re.fullmatch(
+                    pattern, decoded_base64_authorization_header.strip())
+            if field_match is not None:
+                user = field_match.group('user')
+                password = field_match.group('password')
+                return user, password
+        return None, None
