276e01845fbf7165cb8007ab07366539f613c81e
Oluwasegun Oyelola <seguncrown2009@gmail.com>
2024-04-26 07:02:20 +0100

    Authentication file template updated with required paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 '''
 Authentication file
 '''
+import re
 from flask import request
 from typing import List, TypeVar
 
@@ -14,7 +15,19 @@ class Auth:
         '''
         Checks if a path requires authentication
         '''
-        return False
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         '''
